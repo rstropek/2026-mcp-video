55b7b1123c21c0f1000ca70025ce244463e8e469
Rainer Stropek <rainer@software-architects.at>
2025-12-29 11:39:21 +0100

    Add infrastructure for Streamable MCP Server

--- 20-Streamable/AppHost/AppHost.cs
+++ 20-Streamable/AppHost/AppHost.cs
@@ -1,5 +1,6 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects._10_DemoServer>("demo-server");
+builder.AddProject<Projects._20_McpStreamableServer>("mcp-streamable-server");
 
 builder.Build().Run();
